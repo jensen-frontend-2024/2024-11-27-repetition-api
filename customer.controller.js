316dde8bc99ece0cc2f581c47a715a52056d6c3a
Niklas Fähnrich <niklas.fahnrich@safeture.com>
2024-11-27 14:12:39 +0100

    update with nore advanced get query with invoices and a post

--- customer.controller.js
+++ customer.controller.js
@@ -13,4 +13,105 @@ const getAllCustomers = (req, res) => {
   return res.json(customers);
 };
 
-module.exports = { getAllCustomers };
+const getCustomerById = (req, res) => {
+  const { id } = req.params;
+
+  // parseInt will return NaN if the parsing doesn't work. NaN is always falsy.
+  if (!parseInt(id)) {
+    return res.status(400).json({ message: "Id must be a number" });
+  }
+
+  const query = `SELECT * FROM customers WHERE CustomerId = ?`;
+  const stmt = db.prepare(query);
+  const customer = stmt.get([id]);
+
+  if (!customer) {
+    return res
+      .status(404)
+      .json({ message: `Customer with id ${id} doesn't exist.` });
+  }
+
+  return res.json(customer);
+};
+
+const getCustomerByIdWithInvoices = (req, res) => {
+  // ########## Get customer first ##########
+  const { id } = req.params;
+
+  if (!parseInt(id)) {
+    return res.status(400).json({ message: "Id must be a number" });
+  }
+
+  const query = `SELECT CustomerId, FirstName, LastName FROM customers WHERE CustomerId = ?`;
+  const stmt = db.prepare(query);
+  const customer = stmt.get([id]);
+
+  if (!customer) {
+    return res
+      .status(404)
+      .json({ message: `Customer with id ${id} doesn't exist.` });
+  }
+
+  // ########## Get invoices on the customer ##########
+
+  const invoiceQuery = `SELECT InvoiceId, Total FROM invoices WHERE CustomerId = ?`;
+  const invoiceStmt = db.prepare(invoiceQuery);
+  const invoices = invoiceStmt.all([id]);
+
+  if (invoices.length === 0) {
+    return res
+      .status(404)
+      .json({ message: `Customer with id ${id}, doesn't have any invoices` });
+  }
+
+  const customerWithInvoices = {
+    ...customer, // Will copy all the key-value pairs from the customer object
+    invoices, // invoices: invoices
+  };
+
+  return res.json(customerWithInvoices);
+
+  // ########## Put them together and send a response ##########
+};
+
+const postCustomer = (req, res) => {
+  const { body } = req;
+  const { FirstName, LastName, Email } = body;
+
+  // Ternary operator
+  const bodyIsComplete = FirstName && LastName && Email ? true : false;
+
+  if (bodyIsComplete === false) {
+    return res
+      .status(400)
+      .json({ message: "Body is not complete or malformed" });
+  }
+
+  const query = `
+    INSERT INTO customers (Firstname, LastName, Email)
+    VALUES (?, ?, ?)
+  `;
+
+  const stmt = db.prepare(query);
+
+  // lastInsertRowId is something the db returns to us after a successfull creation. We can use this to get the newly create customer.
+  const { lastInsertRowid } = stmt.run([FirstName, LastName, Email]);
+
+  const customerQuery = `SELECT * FROM customers WHERE CustomerId = ?`;
+  const customerStmt = db.prepare(customerQuery);
+  const customer = customerStmt.get([lastInsertRowid]);
+
+  return res
+    .status(201)
+    .json({
+      message: "Customer was successfully created",
+      newCustomer: customer,
+    });
+};
+
+module.exports = {
+  getAllCustomers,
+  getCustomerById,
+  getCustomerByIdWithInvoices,
+  postCustomer,
+};
--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const { getAllCustomers } = require("./customer.controller.js");
+const {
+  getAllCustomers,
+  getCustomerById,
+  getCustomerByIdWithInvoices,
+  postCustomer,
+} = require("./customer.controller.js");
 
 const express = require("express");
 const app = express();
+
+// Makes sure there is a body attribute on the req object
+app.use(express.json());
 
 app.get("/", (req, res) => {
   return res.send("App is running!");
 });
 
 app.get("/customers", getAllCustomers);
+app.get("/customers/:id", getCustomerById);
+app.get("/customers/:id/with-invoices", getCustomerByIdWithInvoices);
+app.post("/customers", postCustomer);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
